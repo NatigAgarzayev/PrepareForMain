7799fce1a7b1cc9fdb2a83628910295f0a124987
natigGit <moothman@inbox.ru>
2023-02-05 19:53:57 +0400

    add tailwind and login page

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
+import './index.css';
 import App from './App';
 import {
     createBrowserRouter,
     RouterProvider,
   } from "react-router-dom";
+import Login from './pages/login/Login';
 
   const router = createBrowserRouter([
     {
       path: "/",
       element: <App />,
-      children
     },
+    {
+      path: "/login",
+      element: <Login />
+    }
   ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
-
+import './Login.scss'
+import logoAuth from '../../images/logo-auth.svg'
+import loginBg from '../../images/login-bg.svg'
 function Login() {
     return (
-        <div className=''>
-            asd
+        <div className='login'>
+            <div className="md:container mx-auto px-5">
+                <nav className='navbar'>
+                    <div className="logo">
+                        <img src={logoAuth} alt="Connect" />
+                        <div className="logo__content">
+                            <h2>Connect</h2>
+                            <small>Express yourself everyday</small>
+                        </div>
+                    </div>
+                    <div className="nav__btn">Sign Up</div>
+                </nav>
+                <div className="login__content">
+                    <div className="login__form">
+                        <h2>Enter Credentials</h2>
+                        <small>Please sign in to continue</small>
+                        <form>
+                            <input className='login__input' type="text" placeholder='Username' />
+                            <input className='login__input' type="password" placeholder='Password' />
+                            <div className="login__offer">
+                                <a href="/" className="login__offer-item">Forgot Password?</a>
+                                <p>Don’t have an account? <a href="/" className="login__offer-item">Sign Up</a></p>
+                            </div>
+                            <button className='login__btn'>Login</button>
+                        </form>
+                    </div>
+                    <div className="login__image">
+                        <img src={loginBg} alt="" />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
